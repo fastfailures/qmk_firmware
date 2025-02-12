193404a4a163aab465df2e19324acde651cb3627
Xandre Ibuquil <ibuquil@cryptolab.net>
2025-02-12 20:54:45 +0100

    keychronv2: first draft

---
+++ keyboards/keychron/v2/ansi_encoder/keymaps/pseudoergo/rules.mk
@@ -0,0 +1,9 @@
+ENCODER_MAP_ENABLE = yes
+
+# Mouse Key
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_mouse_keys.md
+MOUSEKEY_ENABLE = yes
+
+# Caps Word
+# https://github.com/qmk/qmk_firmware/blob/master/docs/features/caps_word.md
+CAPS_WORD_ENABLE = yes
